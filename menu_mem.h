28ba40ce07097089d7f2a81f731b599968c307e9
Felix Erckenbrecht <dg1yfe@stus-disco.de>
2012-07-15 15:29:07 +0200

    Included menu_mem.h Header file

---
+++ menu_mem.h
@@ -0,0 +1,17 @@
+/*
+ * menu_mem.h
+ *
+ *  Created on: 13.07.2012
+ *      Author: F. Erckenbrecht
+ */
+
+#ifndef MENU_MEM_H_
+#define MENU_MEM_H_
+
+void m_recall_submenu(char key);
+void m_store_submenu(char key);
+void m_show_slot();
+void m_mem_select(char key);
+
+
+#endif /* MENU_MEM_H_ */
